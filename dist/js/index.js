ea3f52a289a369be488d444c30c0e3d9b2ed8ba2
Bruce <bruce8046@gmail.com>
2023-04-03 16:46:04 +0800

    JS載入畫面

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,11 +1,11 @@
 console.log("Hello");
 // 載入畫面
 let percent=0;
-let bar=document.querySelector("#bar");
+let br=document.querySelector("#bar");
 let pl=document.querySelector("#pageloading")
 // console.log(bar);
 let timer=setInterval(function(){
-    bar.style.width=percent+"%";
+    br.style.width=percent+"%";
     percent+=1;
     if(percent>100){
         pl.classList.add("complete");
