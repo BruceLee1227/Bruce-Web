d8f1f27c5caedab9dc2ab321df1d88a322cb81cf
Bruce <bruce8046@gmail.com>
2023-04-04 14:46:14 +0800

    RWD

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,14 +1,14 @@
-console.log("Hello");
-// 載入畫面
-let percent=0;
-let br=document.querySelector("#bar");
-let pl=document.querySelector("#pageloading")
-// console.log(bar);
-let timer=setInterval(function(){
-    br.style.width=percent+"%";
-    percent+=1;
-    if(percent>100){
-        pl.classList.add("complete");
-    }
-},60);
-// clearTimeout(timer);
+// console.log("Hello");
+// // 載入畫面
+// let percent=0;
+// let br=document.querySelector("#bar");
+// let pl=document.querySelector("#pageloading")
+// // console.log(bar);
+// let timer=setInterval(function(){
+//     br.style.width=percent+"%";
+//     percent+=1;
+//     if(percent>100){
+//         pl.classList.add("complete");
+//     }
+// },60);
+// // clearTimeout(timer);
