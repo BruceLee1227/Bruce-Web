b53dadf8101698e8160679377fdac64cb37ec99e
Bruce <bruce8046@gmail.com>
2023-04-09 11:38:42 +0800

    Final

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,13 +1,13 @@
-console.log("Hello");
+// console.log("Hello");
 // 載入畫面
-// let percent=0;
-// let br=document.querySelector("#bar");
-// let pl=document.querySelector("#pageloading")
-// // console.log(bar);
-// let timer=setInterval(function(){
-//     br.style.width=percent+"%";
-//     percent+=1;
-//     if(percent>100){
-//         pl.classList.add("complete");
-//     }
-// },60);
+let percent=0;
+let br=document.querySelector("#bar");
+let pl=document.querySelector("#pageloading")
+// console.log(bar);
+let timer=setInterval(function(){
+    br.style.width=percent+"%";
+    percent+=1;
+    if(percent>100){
+        pl.classList.add("complete");
+    }
+},60);
